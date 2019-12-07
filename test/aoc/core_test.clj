0ea571520d6c07b313169b7e3763c8beaa170d82
stuart <qmstuart@gmail.com>
2019-12-07 22:45:47 +0000

    Day 3 Part 1 Solved.

--- test/aoc/core_test.clj
+++ test/aoc/core_test.clj
@@ -1,7 +1,11 @@
 (ns aoc.core-test
   (:require [clojure.test :refer :all]
             [aoc.day1 :refer :all]
-            [aoc.day2 :refer :all]))
+            [aoc.day2 :refer :all]
+            [aoc.day3 :refer :all]))
+
+;; (require 'aoc.core-test :reload-all)
+;; (run-tests 'aoc.core-tests)
 
 ;; Day 1
 
@@ -54,3 +58,31 @@
 (deftest get-int-computer-output-5
   (testing "Should get output of 30 for specified program")
   (is (= 30 (intcode-computer [1 1 1 4 99 5 6 0 99]))))
+
+;; Day 3 Part 1
+
+(deftest get-intermediate-path-1
+  (testing "Should get the correct path")
+  (is (= (set [[1 0] [2 0] [3 0] [4 0] [5 0]])
+         (set (get-intermediate-path [0 0] \R 5)))))
+
+(deftest get-intermediate-path-2
+  (testing "Should get the correct path")
+  (is (= (set [[-1 0] [-2 0] [-3 0] [-4 0] [-5 0]])
+         (set (get-intermediate-path [0 0] \L 5)))))
+
+(deftest get-intermediate-path-3
+  (testing "Should get the correct path")
+  (is (= (set [[0 1] [0 2] [0 3] [0 4] [0 5]])
+         (set (get-intermediate-path [0 0] \U 5)))))
+
+(deftest get-intermediate-path-4
+  (testing "Should get the correct path")
+  (is (= (set [[0 -1] [0 -2] [0 -3] [0 -4] [0 -5]])
+         (set (get-intermediate-path [0 0] \D 5)))))
+
+
+(deftest get-intermediate-path-5
+  (testing "Should get the correct path")
+  (is (= (set [[20 11] [20 12] [20 13] [20 14] [20 15]])
+         (set (get-intermediate-path [20 10] \U 5)))))
