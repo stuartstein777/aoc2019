a625048d250e88a9b311095bb00f078151c8fd85
stuart <qmstuart@gmail.com>
2019-12-09 23:44:22 +0000

    Day 8 Part 2 Solved.

--- src/aoc/day8.clj
+++ src/aoc/day8.clj
@@ -42,5 +42,67 @@
         zero-counts         (map count-zeroes input)
         smallest-zero-count (apply min zero-counts)
         index               (.indexOf zero-counts smallest-zero-count)]
+    (println smallest-zero-count)
     (* (count (filter #(= \2 %) (nth input index)))
        (count (filter #(= \1 %) (nth input index))))))
+
+;; --- Part Two ---
+;
+; Now you're ready to decode the image. The image is rendered by stacking the layers and aligning the pixels with the
+; same positions in each layer. The digits indicate the color of the corresponding pixel: 0 is black, 1 is white, and
+; 2 is transparent.
+;
+; The layers are rendered with the first layer in front and the last layer in back. So, if a given position has a
+; transparent pixel in the first and second layers, a black pixel in the third layer, and a white pixel in the fourth
+; layer, the final image would have a black pixel at that position.
+;
+; For example, given an image 2 pixels wide and 2 pixels tall, the image data 0222112222120000 corresponds to the
+; following image layers:
+;
+; Layer 1: 02
+;          22
+;
+; Layer 2: 11
+;          22
+;
+; Layer 3: 22
+;          12
+;
+; Layer 4: 00
+;          00
+;
+; Then, the full image can be found by determining the top visible pixel in each position:
+;
+;    The top-left pixel is black because the top layer is 0.
+;    The top-right pixel is white because the top layer is 2 (transparent), but the second layer is 1.
+;    The bottom-left pixel is white because the top two layers are 2, but the third layer is 1.
+;    The bottom-right pixel is black because the only visible pixel in that position is 0 (from layer 4).
+;
+; So, the final image looks like this:
+;
+; 01
+; 10
+;
+; What message is produced after decoding your image?
+
+(defn get-pixel [coll loc]
+  (let [pixel (nth (first coll) loc)]
+    (if (= pixel \2)
+      (recur (rest coll) loc)
+      (cond (= pixel \1) \▓
+            :else \░))))
+
+(map ( partial get-pixel (partition 4 "0222112222120000")) (range 4))
+;=> (\0 \1 \1 \0)
+
+(partition 25 (map (partial get-pixel (partition 150 (slurp "resources/day8_input"))) (range 150)))
+
+;((\▓ \▓ \▓ \▓ \░ \░ \▓ \▓ \░ \░ \░ \░ \▓ \▓ \░ \▓ \░ \░ \▓ \░ \▓ \▓ \▓ \▓ \░)
+; (\▓ \░ \░ \░ \░ \▓ \░ \░ \▓ \░ \░ \░ \░ \▓ \░ \▓ \░ \░ \▓ \░ \░ \░ \░ \▓ \░)
+; (\▓ \▓ \▓ \░ \░ \▓ \░ \░ \░ \░ \░ \░ \░ \▓ \░ \▓ \░ \░ \▓ \░ \░ \░ \▓ \░ \░)
+; (\▓ \░ \░ \░ \░ \▓ \░ \▓ \▓ \░ \░ \░ \░ \▓ \░ \▓ \░ \░ \▓ \░ \░ \▓ \░ \░ \░)
+; (\▓ \░ \░ \░ \░ \▓ \░ \░ \▓ \░ \▓ \░ \░ \▓ \░ \▓ \░ \░ \▓ \░ \▓ \░ \░ \░ \░)
+; (\▓ \░ \░ \░ \░ \░ \▓ \▓ \▓ \░ \░ \▓ \▓ \░ \░ \░ \▓ \▓ \░ \░ \▓ \▓ \▓ \▓ \░))
+
+
+
